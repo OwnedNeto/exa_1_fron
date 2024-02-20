0edc0c4c09fc9ac5810f3b4a675b0fa7e786d8ae
OwnedNeto <melendezernesto777@gmail.com>
2024-02-20 13:08:20 -0600

    examen_u1

---
+++ exa.js
@@ -0,0 +1,39 @@
+
+
+    const nombres =(nombres) =>{
+
+        const {ernesto, lisando, angel} = nombres;
+
+        return(nombres);
+        console.log(ernesto);
+        console.log(lisando);
+        console.log(angel);
+
+
+    }
+
+
+    const arreglos = (arreglo)  => {
+
+        const nombres = [{
+            name1: ernesto, 
+            name2:marlon,
+            name3: lisandro}],
+            [{
+                name22: angel,
+                name23: dedo,
+            }];
+
+            nombres.map(({name1, name2, name3} = persona) => {
+                console.log('los datos son :');
+            }
+        
+    };
+
+
+
+
+
+
+
+
